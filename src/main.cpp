1ebad3dce9a1e50dacf6597457a6721ef624260a
CloudOn9 <thealtfulthing@tutanota.com>
2022-01-28 17:04:29 -0800

    Create binary implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,66 @@
 #include <algorithm>
-#include <bitset>
+#include <cmath>
 #include <iostream>
 #include <vector>
 
-void b2()
+void b10()
 {
-    std::cout << "Enter the numbers one line at a time. Enter '2' to indicate that you are done.\n";
+    using std::cin, std::cout;
 
-    std::vector<char> input;
+    cout << "Enter the numbers, seperating them with a line or a space."
+              << "Enter '0' to indicate that you are done.\n";
+    std::vector<char> chars;
 
     while (true)
     {
         int in;
-        std::cin >> in;
-        //TODO Add binary conversion.
+        cin >> in;
+        if (in == 0)
+            break;
+
+        chars.push_back(in);
+    }
+
+    for (auto character : chars)
+    {
+        cout << character; //No newline.
+    }
+}
+
+void b2()
+{
+    using std::cin, std::cout;
+
+    cout << "Enter the numbers, seperating them with a line or a space."
+              << "Enter '2' to indicate that you are done.\n";
+    std::vector<char> chars;
+
+    while (true)
+    {
+        int in, remainder{}, decimal_equiv{}, power{0};
+        cin >> in;
 
         if (in == 2)
             break;
 
-        input.push_back(in);
+        while (in != 0)
+        {
+            remainder = in % 10;
+            in /= 10;
+            decimal_equiv += remainder * pow(2, power);
+            ++power;
+        }
+
+        chars.push_back(decimal_equiv);
     }
 
-    for (auto character : input)
+    for (auto character : chars)
     {
-        std::cout << character; //No newline.
+        cout << character; //No newline.
     }
 }
 
-void b10()
-{
-    std::cout << "Enter the numbers one line at a time. Enter '0' to indicate that you are done.\n";
-    std::vector<char> input;
-
-    while (true)
-    {
-        int in;
-        std::cin >> in;
-        if (in == 0)
-            break;
-
-        input.push_back(in);
-    }
-
-    for (auto character : input)
-    {
-        std::cout << character; //No newline.
-    }
-}
-
-void b8() 
+void b8()
 {
     std::cin >> std::oct;
     b10();
@@ -67,31 +79,32 @@ void b64()
 
 int main()
 {
+    using std::cin, std::cout, std::string;
 
-    std::cout << "Enter desired mode.\n";
-    //std::cout << "Bin: Base 2 (Binary) to letters.\n";
-    std::cout << "O: Base 8 (Octal) to letters.\n";
-    std::cout << "D: Base 10 (Decimal) to letters.\n";
-    std::cout << "H: Base 16 (Hex) to letters.\n";
+    cout << "Enter desired mode.\n";
+    cout << "B: Base 2 (Binary) to letters.\n";
+    cout << "O: Base 8 (Octal) to letters.\n";
+    cout << "D: Base 10 (Decimal) to letters.\n";
+    cout << "H: Base 16 (Hex) to letters.\n";
     //std::cout << "B64: Base 64 to letters.\n";
 
-    std::string input;
-    std::cin >> input;
+    string input;
+    cin >> input;
 
     transform(input.begin(), input.end(), input.begin(), ::tolower);
 
-    /*if (input == "bin")
+    if (input == "b")
         b2();
-    else*/ if (input == "o")
+    else if (input == "o")
         b8();
     else if (input == "d")
         b10();
     else if (input == "h")
         b16();
     //else if (input == "b64")
-        //b64();
+    //b64();
     else
-        std::cout << "Invalid input. Shutting down...";
+        cout << "Invalid input. Shutting down...";
 
     return 0;
 }
